28764f68c0db44382f47e8a6b5a03eb2410330d3
Aidan Middleton <aidanemiddleton@gmail.com>
2020-06-04 19:43:02 -0700

    added projectlist and projectlistitem components

---
+++ src/components/project_unit/Project_List_Item.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function ProjectListItem(props) {
+
+
+  return (
+  <div className='project-list-item'>
+    <img className='profile-pic' src='https://github.com/aidanmiddleton/professional_portfolio/blob/master/src/images/IMG_0294.png?raw=true'></img>
+    <h2>Aidan Middleton</h2>
+    <h4>aidanemiddleton@gmail.com</h4>
+  </div>
+  );
+}
