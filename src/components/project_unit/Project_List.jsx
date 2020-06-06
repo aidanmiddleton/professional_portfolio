eb5dec6e666ee292dc378811ba60c93d8b12d2e0
Aidan Middleton <aidanemiddleton@gmail.com>
2020-06-05 18:51:30 -0700

    added connection to data array for projects

--- src/components/project_unit/Project_List.jsx
+++ src/components/project_unit/Project_List.jsx
@@ -1,47 +1,58 @@
 import React from 'react';
 import ProjectListItem from './Project_List_Item';
 
+const projectsData = [
+
+  {
+    name: "Professional Portfolio",
+    description: "My own personal portfolio website, built to display projects, and allow for easy contact.",
+    stack: "React.js, Node.js,",
+    youtube_url: "https://www.youtube.com/watch?v=Yxrrb0am988",
+    github_url: "https://github.com/aidanmiddleton/professional_portfolio"
+
+  },
+
+  {
+    name: "Dungeons and Dragons Character Creator",
+    description: "A DnD charcter creation/companion app. Allows for the creation of charatacters, and viewing of previously created characters.\n The user can also roll for intiative, and make attack rolls",
+    stack: "React.js, Node.js, Express, postgres, ElephantSQL",
+    youtube_url: "https://www.youtube.com/watch?v=Yxrrb0am988",
+    github_url: "https://github.com/hernandez87v/dnd-character-create"
+
+  },
+
+  {
+    name: "Jungle",
+    description: "Jungle is a basic Ecommerce application created using ruby on rails.",
+    stack: "Ruby, Rails, ERB, postgres, Jest",
+    youtube_url: "https://www.youtube.com/watch?v=Yxrrb0am988",
+    github_url: "https://github.com/aidanmiddleton/Jungle"
+
+  },
+
+
+];
+
+
 export default function ProjectList(props) {
-
-
-  const projectsData = [
-
-    {
-      name: "Professional Portfolio",
-      description: "My own personal portfolio website, built to display projects, and allow for easy contact.",
-      stack: "React.js, Node.js,",
-      youtubeURL: "https://www.youtube.com/watch?v=Yxrrb0am988",
-      github_url: "https://github.com/aidanmiddleton/professional_portfolio"
-
-    },
-
-    {
-      name: "Dungeons and Dragons Character Creator",
-      description: "A DnD charcter creation/companion app. Allows for the creation of charatacters, and viewing of previously created characters.\n The user can also roll for intiative, and make attack rolls",
-      stack: "React.js, Node.js, Express, postgres, ElephantSQL",
-      youtubeURL: "https://www.youtube.com/watch?v=Yxrrb0am988",
-      github_url: "https://github.com/hernandez87v/dnd-character-create"
-
-    },
-
-    {
-      name: "Jungle",
-      description: "Jungle is a basic Ecommerce application created using ruby on rails.",
-      stack: "Ruby, Rails, ERB, postgres, Jest",
-      youtubeURL: "https://www.youtube.com/watch?v=Yxrrb0am988",
-      github_url: "https://github.com/aidanmiddleton/Jungle"
-
-    },
-
-
-  ];
+  
+  let projects = projectsData.map(project => {
+    return (
+      <ProjectListItem 
+        name={project.name}
+        description={project.description} 
+        stack={project.stack}
+        youtubeURL={project.youtube_url}
+        githubURL={project.github_url}  
+      />
+    )
+  })
+  
 
   return (
     <container className='project-container'>
       <ul className='project-list'>
-        <ProjectListItem />
-        <ProjectListItem />
-        <ProjectListItem />
+        {projects}
       </ul>
     </container>
   );
--- src/components/project_unit/Project_List_Item.jsx
+++ src/components/project_unit/Project_List_Item.jsx
@@ -6,8 +6,10 @@ export default function ProjectListItem(props) {
 
   return (
   <li className='project-list-item'>
-    <h1 className='project-item-title'>Jungle</h1>
-    <p>Ruby on Rails</p>
+    <h1 className='project-item-title'>{props.name}</h1>
+    <p>{props.description}</p>
+    <p>{props.stack}</p>
+    <a to={props.githubURL}></a>
     <ReactPlayer
         url="https://www.youtube.com/watch?v=Yxrrb0am988"
     />
