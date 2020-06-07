26a19810729323ff45ad0f03e979d4f48ddd9813
Aidan Middleton <aidanemiddleton@gmail.com>
2020-06-07 12:39:10 -0700

    more styling changes to projects page

--- src/components/project_unit/Project_List_Item.jsx
+++ src/components/project_unit/Project_List_Item.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import ReactPlayer from 'react-player';
+import { SocialIcon } from 'react-social-icons';
 
 export default function ProjectListItem(props) {
 
 
   return (
   <li className='project-list-item'>
-    <h1 className='project-item-title'>{props.name}</h1>
-    <p>{props.description}</p>
-    <p>{props.stack}</p>
-    <a to={props.githubURL}></a>
-    <ReactPlayer
-        url="https://www.youtube.com/watch?v=Yxrrb0am988"
-    />
+    <div className='project-item-div'>
+
+      <h1 className='project-item-title'>{props.name}</h1>
+      <p>{props.description}</p>
+      <p>{props.stack}</p>
+      <SocialIcon className='social-icon' url={props.githubURL} />
+      <ReactPlayer
+          url="https://www.youtube.com/watch?v=Yxrrb0am988"
+      />
+
+    </div>
   </li>
   );
 }
